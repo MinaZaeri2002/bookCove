03e291c580c849ef8439bfbb46cade8c7fa8021b
minaZaeri2002 <minazaeri2002@gmail.com>
2025-06-29 22:46:15 +0330

    feat(accounts): Implement user signup feature
    Adds the complete user registration flow. This includes the
    SignUpView, an updated CustomUserCreationForm to support the UI,
    the responsive signup.html template with custom CSS/JS, and
    the necessary URL routing.

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,50 +7,28 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(
         required=True,
-        widget=forms.EmailInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'آدرس ایمیل',
-        })
     )
 
     first_name = forms.CharField(
         max_length=30,
         required=False,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'نام'
-        })
     )
 
     last_name = forms.CharField(
         max_length=30,
         required=False,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'نام خانوادگی'
-        })
     )
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+        fields = ('username', 'email', 'first_name', 'last_name')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'نام کاربری'
-        })
 
-        self.fields['password1'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'رمز عبور'
-        })
-
-        self.fields['password2'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'تایید رمز عبور'
-        })
+        for field_name, field in self.fields.items():
+            field.widget.attrs['placeholder'] = ' '
+            field.label = ''
 
     def save(self, commit=True):
         user = super().save(commit=False)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .forms import UserProfileForm, LoginForm
 
-# Create your views here.
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('login')
+
+
+
